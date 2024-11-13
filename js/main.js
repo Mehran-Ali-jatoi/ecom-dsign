52b42de24bf5711880d16fc0ee6597309d9226ab
Mehran-Ali-jatoi <mehran.jato.i258@gmail.com>
2024-11-14 00:09:03 +0500

    Creat a ecomerce design

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+let bar =document.getElementById("bar");
+let nav_ul = document.getElementById("nav_ul")
+let bars = document.getElementById("nav_ull")
+bar.addEventListener("click",()=>{
+    nav_ul.classList.toggle("top")
+    nav_ul.classList.toggle("any")
+
+    
+})
+bars.addEventListener("click",()=>{
+    nav_ul.classList.toggle("top")
+    nav_ul.classList.toggle("any")
+    
+})
